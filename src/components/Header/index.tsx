be54929bbee6979c7c2bff29ca8c8787029dcb97
Kevin Willian <kevinwillian084@gmail.com>
2022-10-07 02:14:36 -0300

    feat: adiciona component header estilizado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+export const Header = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="kw money" />
+        <button type="button">
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  );
+};
