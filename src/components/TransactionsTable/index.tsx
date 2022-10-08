060050831779310f613b4b1c856e6c56bf88ca36
Kevin Willian <kevinwillian084@gmail.com>
2022-10-08 16:44:31 -0300

    feat: Adiciona lógica de buscar os dados diretamente da fakeAPI e adiciona formatacao de textos

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,11 @@
+import { useTransactions } from "../../context/TransactionContext";
 import { Container, TableContent } from "./styles";
 
 export const TransactionsTable = () => {
+  const { transactions } = useTransactions();
+  if (!transactions) {
+    return <p>Carregando...</p>;
+  }
   return (
     <Container>
       <TableContent>
@@ -13,30 +18,24 @@ export const TransactionsTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/10/2022</td>
-          </tr>
-          <tr>
-            <td>Compra mensal</td>
-            <td className="withdraw">- R$ 1.000</td>
-            <td>Casa</td>
-            <td>05/10/2022</td>
-          </tr>
-          <tr>
-            <td>Shopping</td>
-            <td className="withdraw">- R$ 1.000</td>
-            <td>Pessoal</td>
-            <td>20/10/2022</td>
-          </tr>
-          <tr>
-            <td>Cinema</td>
-            <td className="withdraw">- R$ 150,00</td>
-            <td>Pessoal</td>
-            <td>20/10/2022</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {transaction.type === "withdraw" && "- "}
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </TableContent>
     </Container>
