8a525b148f93a2ce3990ba588ec29eb2072166da
Kevin Willian <kevinwillian084@gmail.com>
2022-10-08 16:38:12 -0300

    feat: Adiciona seeds  de Transactions para a fake api

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,55 @@
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { TransactionProvider } from "./context/TransactionContext";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Freelance de website",
+          type: "withdraw",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 3,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title:
-            "Someday ill be falling without caution but for now im onlyyyyy... people watching",
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
     });
   },
 });
@@ -23,7 +57,7 @@ createServer({
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <TransactionProvider>
-    <App />
+      <App />
     </TransactionProvider>
   </React.StrictMode>
 );
