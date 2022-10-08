296cf26e9807f9fd5a6aeacedd39f088e8adb43a
Kevin Willian <kevinwillian084@gmail.com>
2022-10-07 21:08:52 -0300

    feat: Adiciona libs: axios, polished miragejs e radix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createServer } from "miragejs";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title:
+            "Someday ill be falling without caution but for now im only... people watching",
+        },
+      ];
+    });
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
