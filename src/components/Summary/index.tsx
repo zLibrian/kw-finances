174a11eca4c2eec22b5602952c67146c99a9d725
Kevin Willian <kevinwillian084@gmail.com>
2022-10-08 21:36:37 -0300

    feat: Adiciona logicade somar os totais das transactions em tela

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,29 @@
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
+import { useTransactions } from "../../context/TransactionContext";
+import { formatPrice } from "../../helpers";
 import { Container, SummaryCard } from "./styles";
 
 export const Summary = () => {
+  const { transactions } = useTransactions();
+  const summaryTotal = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws -= transaction.amount;
+        acc.total -= transaction.amount;
+      }
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
   return (
     <Container>
       <SummaryCard>
@@ -11,21 +31,21 @@ export const Summary = () => {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$ 1000,00</strong>
+        <strong>{formatPrice(summaryTotal.deposits)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <p>Saidas</p>
           <img src={outcomeImg} alt="Saidas" />
         </header>
-        <strong>R$ 200,00</strong>
+        <strong>{formatPrice(summaryTotal.withdraws)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$ 800,00</strong>
+        <strong>{formatPrice(summaryTotal.total)}</strong>
       </SummaryCard>
     </Container>
   );
